89082970194b0e0f2c62dfb1a322776b303ae1c1
Filip Dusek <fdusek1@gmail.com>
2018-09-16 11:07:45 +0200

    Store rewards and actions

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-./tools/playgame.py --nolaunch --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 100 --map_file tools/maps/example/tutorial1.map "$@" "python MyBot.py" "python tools/sample_bots/python/LeftyBot.py" #"python tools/sample_bots/python/HunterBot.py" "python tools/sample_bots/python/GreedyBot.py"
+./tools/playgame.py --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 100 --map_file tools/maps/example/tutorial1.map "$@" "python MyBot.py" "python tools/sample_bots/python/LeftyBot.py"
 echo "\n\nContent of debug log:"
 cat ./debug.txt
