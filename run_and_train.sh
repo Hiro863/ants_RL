345c0252620cf608271046b0b35d4e0aaa1949a2
Hiro <hirofeynman@gmail.com>
2018-09-24 22:55:52 +0200

    Preprocessing fixed

--- run_and_train.sh
+++ run_and_train.sh
@@ -1,11 +1,20 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash#!/usr/bin/env sh
+#echo Observing...
+#python3 TrainSession.py observing
 
-END=1000
-for i in $(seq 1 $END); do
-    echo $i
+iteration=5000
+gameplay=10
+for i in $(seq 1 $iteration); do
+
+    for j in $(seq 1 $gameplay); do
+    echo Iteration: $i, Gameplay: $j
     ./tools/playgame.py  --nolaunch --player_seed 42 --end_wait=0.25 --log_dir game_logs --turns 1000 --map_file tools/maps/example/tutorial1.map "$@" "python3 MyBot.py" "python tools/sample_bots/python/LeftyBot.py" #"python tools/sample_bots/python/HunterBot.py" "python tools/sample_bots/python/GreedyBot.py"
     echo "\n\nContent of debug log:"
     cat ./debug.txt
+    done
+
+    python3 TrainSession.py
     python3 save_results.py
-    python3 TrainSession.py training
+    python3 visualise.py
+    rm training.p
 done
